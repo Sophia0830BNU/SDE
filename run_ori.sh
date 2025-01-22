2e78afda33846052ec3e3a2c789f09ad27feca9c
Feifei Qian <140052100+Sophia0830BNU@users.noreply.github.com>
2025-01-22 14:51:00 +0800

    Update run_ori.sh

--- run_ori.sh
+++ run_ori.sh
@@ -1,5 +1,5 @@
 
-nohup python3 -u main2_10times_degree.py --dataset MUTAG --device 3 --batch_size 32 --epochs 350 --lr 0.01 --num_layers 5 --num_mlp_layers 1 --hidden_dim 32 --final_dropout 0.5 --graph_pooling_type sum --neighbor_pooling_type sum  --learn_eps >>MUTAG.txt
+nohup python3 -u main.py --dataset MUTAG --device 3 --batch_size 32 --epochs 350 --lr 0.01 --num_layers 5 --num_mlp_layers 1 --hidden_dim 32 --final_dropout 0.5 --graph_pooling_type sum --neighbor_pooling_type sum  --learn_eps >>MUTAG.txt
 
 
 
