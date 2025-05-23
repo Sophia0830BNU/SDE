9ea0748237261463ef161a7f6e5ec2454c8b24cb
Feifei Qian <140052100+Sophia0830BNU@users.noreply.github.com>
2025-05-23 16:26:18 +0800

    Delete run_ori.sh

--- run_ori.sh
+++
@@ -1,7 +0,0 @@
-
-nohup python3 -u main.py --dataset MUTAG --device 3 --batch_size 32 --epochs 350 --lr 0.01 --num_layers 5 --num_mlp_layers 1 --hidden_dim 32 --final_dropout 0.5 --graph_pooling_type sum --neighbor_pooling_type sum  --learn_eps >>MUTAG.txt
-
-
-
-
-
